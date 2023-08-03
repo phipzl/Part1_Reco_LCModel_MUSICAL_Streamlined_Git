da71f858b256d032a20c15209097234f72ef2b1f
Bernhard Strasser <bernhard.strasser@meduniwien.ac.at>
2023-08-03 11:24:10 +0200

    Removed all old Matlab-functions.

--- Matlab_Functions/AdditionalMatlabFunctions/TestMemoryFunction1.m
+++
@@ -1,8 +0,0 @@
-function y1 = TestMemoryFunction1(x1)
-
-    x1(1) = 5;
-    y1 = TestMemoryFunction2(x1);
-    y1(1) = 1;
-    y1(2) = 2;
-
-end
--- Matlab_Functions/MatlabSourceCode_3rdParty/Lipid_Suppression_Toolbox/@FT_v2/FT_v2.m
+++
@@ -1,7 +0,0 @@
-function res = FT_v2(mask)
-
-res.adjoint = 0;
-res.mask = mask;
-res = class(res,'FT_v2');
-
-end
