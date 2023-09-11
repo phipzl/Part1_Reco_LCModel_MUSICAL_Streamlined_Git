e79277788f70f42d07caf65590cf7bb9da4f2995
Bernhard Strasser <bernhard.strasser@meduniwien.ac.at>
2023-09-11 16:27:38 +0200

    1) Change from Philip Lazen in Split_DICOM_Folder.sh: Changed "two-digit number" to "sequence number". 2) Change from Philipp Lazen in create_mask.sh: Actually use the Split_DICOM_Folder.sh now.

--- Split_DICOM_Folder.sh
+++ Split_DICOM_Folder.sh
@@ -41,9 +41,9 @@ files=("$folder"/*.MR.*.00??.????.??.??.*.IMA)
 # Initialize variables to store the first two-digit number encountered
 first_number=""
 
-# Iterate over the files and extract the two-digit number
+# Iterate over the files and extract the sequence number
 for file in "${files[@]}"; do
-    # Extract the two-digit number using awk
+    # Extract the sequence number using awk
 	number=$(basename "$file" | awk -F'[._]' '{print $8}')
     # If it's the first number encountered, store it
     if [ -z "$first_number" ]; then
@@ -52,11 +52,11 @@ for file in "${files[@]}"; do
  
     # Check if the current number is different from the first
     if [ "$number" != "$first_number" ]; then
-        echo "Files in $folder have different two-digit numbers."
+        echo "Files in $folder have different sequence numbers."
     	sleep 1
         # Move files with a lower number to folder_2
         mv "$folder"/*.MR.*."$first_number".????.????.??.??.* "$folder_2"/
-        echo "Files with number $first_number moved to $folder_2."
+        echo "Files with sequence number $first_number moved to $folder_2."
         sleep 1
         # Exit the script
         exit 0
@@ -64,5 +64,5 @@ for file in "${files[@]}"; do
 done
 
 # If we reach this point, all files have the same two-digit number
-echo "All files in $folder have the same two-digit number ($first_number)."
+echo "All files in $folder have the same sequence number ($first_number)."
 
