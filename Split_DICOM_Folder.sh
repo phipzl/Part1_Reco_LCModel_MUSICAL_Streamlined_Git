7fc7d1762599589adf5a287559d1ba350d25e904
Philipp Lazen <philipp.lazen@meduniwien.ac.at>
2023-10-19 16:00:07 +0200

    Bugfix in Split_DICOM_Folder.sh

--- Split_DICOM_Folder.sh
+++ Split_DICOM_Folder.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# This is a script that checks the DICOM files in a given folder, 
+# and if they have different series numbers, moves the one with 
+# the LOWER number to another folder (folder_2).
+# Limitations: 
+# If more than two series exist, it moves all beyond the first 
+# series to folder_2. Relies on the series number being in a 
+# specific position. 
+
+
 
 # Function to display script usage
 usage() {
@@ -29,12 +38,6 @@ if [ -z "$folder" ]; then
     usage
 fi
 
-# Create the folder_2 directory next to the specified folder
-folder_2="${folder}_2"
-
-echo $folder_2
-mkdir -p "$folder_2"
-
 # Get a list of all files in the specified folder
 files=("$folder"/*.MR.*.00??.????.??.??.*.IMA)
 
@@ -43,24 +46,28 @@ first_number=""
 
 # Iterate over the files and extract the sequence number
 for file in "${files[@]}"; do
-    # Extract the sequence number using awk
-	number=$(basename "$file" | awk -F'[._]' '{print $8}')
-    # If it's the first number encountered, store it
-    if [ -z "$first_number" ]; then
-        first_number="$number"
-    fi
- 
-    # Check if the current number is different from the first
-    if [ "$number" != "$first_number" ]; then
-        echo "Files in $folder have different sequence numbers."
-    	sleep 1
-        # Move files with a lower number to folder_2
-        mv "$folder"/*.MR.*."$first_number".????.????.??.??.* "$folder_2"/
-        echo "Files with sequence number $first_number moved to $folder_2."
-        sleep 1
-        # Exit the script
-        exit 0
-    fi
+	# Extract the sequence number using awk
+	number=$(basename "$file" | awk -F'[.]' '{print $4}')
+	
+	# If it's the first number encountered, store it
+	if [ -z "$first_number" ]; then
+		first_number="$number"
+	fi
+
+	# Check if the current number is different from the first
+	if [ "$number" != "$first_number" ]; then
+		echo "Files in $folder have different sequence numbers."
+
+		# Create the folder_2 directory next to the specified folder
+		folder_2="${folder}_2"
+		echo $folder_2
+		mkdir -p "$folder_2"
+
+		# Move files with a lower number to folder_2
+		mv "$folder"/*.MR.*."$first_number".????.????.??.??.* "$folder_2"/
+		echo "Files with sequence number $first_number moved to $folder_2."
+		exit 0
+	fi
 done
 
 # If we reach this point, all files have the same two-digit number
